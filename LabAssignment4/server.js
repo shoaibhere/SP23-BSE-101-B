be55c62d73685cf94c01f0443c90da7abd66a46c
shoaibhere <shoaibakhtar321@icloud.com>
2024-12-15 18:19:57 +0500

    Lab Assignment 4 done
    applied searching sorting filtering on brands (accessible by explore our brands button on the main website)

---
+++ LabAssignment4/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+mongoose.set('strictPopulate', false);
+
+const dotenv = require("dotenv");
+
+// Load environment variables from .env.local
+dotenv.config({ path: ".env.local" });
+
+const server = express();
+
+// Middleware setup
+server.use(expressLayouts); // Use layouts for EJS
+server.use(express.json()); // Parse JSON request bodies
+server.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+
+// Static file serving
+server.use(express.static("public"));
+
+// Set view engine to EJS
+server.set("view engine", "ejs");
+
+// Routers
+const adminProductsRouter = require("./routes/admin/product.controller");
+server.use(adminProductsRouter);
+
+const portfolioController = require("./routes/portfolio/portfolio.controller");
+server.use(portfolioController);
+
+const brandController = require("./routes/admin/brand.controller");
+server.use(brandController);
+
+const categoryController = require("./routes/admin/category.controller");
+server.use(categoryController);
+
+const brandsViewController = require("./routes/website/brandsview.controller");
+server.use(brandsViewController);
+
+// Home route
+server.get("/", (req, res) => {
+  res.render("unilever-home");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() =>
+    console.log(
+      "Connected to MongoDB: " + process.env.MONGODB_CONNECTION_STRING
+    )
+  )
+  .catch((error) => console.error("MongoDB Connection Error:", error.message));
+
+// Start the server
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Project started at http://localhost:${PORT}`);
+});
